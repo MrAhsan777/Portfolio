42052894b89d0f0ff23e433ec9f9c0d39bfda7aa
Muhammad Ahsan <150921311+MrAhsan777@users.noreply.github.com>
2025-07-25 11:20:05 +0500

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
     {
       title: 'Face-Detection App',
       description: 'Facial recognition app with streamlit for gui , it has both live and custom features and based on known_faces it detects the faces and tell who he is or if he don't known any faces it simply said unknown face it's still under development but here are some results ',
-      image: 'https://images.unsplash.com/photo-1677442136019-21780ecad995?w=600&h=400&fit=crop',
+      image: '3.jpg',
       technologies: ['Python', 'Yolo V5', 'Stremlit],
       github: 'https://github.com/MrAhsan777/face-detection-app---python',
       demo: 'https://demo.com',
